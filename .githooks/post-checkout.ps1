05958a30bdd8812a5fdd3b7662816e142c1c09f3
nchoquet <nicolachoquet06250@gmail.com>
2025-01-14 16:45:24 +0100

    Fix sur framework, clipboard et norsys

--- .githooks/post-checkout.ps1
+++ .githooks/post-checkout.ps1
@@ -2,33 +2,31 @@ If (-not (Test-Path "$(Get-Location)/bash-aliases-framework")) {
     git clone git@github.com:nicolachoquet06250/bash-aliases-framework.git
 }
 
-Remove-Item -Recurse -Force "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout"
-New-Item -ItemType SymbolicLink -Path "$(Get-Location)/bash-aliases-framework/.githooks/post-checkout.ps1" -Target "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1"
-Set-ExecutionPolicy Bypass -command "$(Get-Location)/bash-aliases-framework/.githooks/post-checkout.ps1"
+If (Test-Path "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1")
+{
+    Remove-Item -Recurse -Force "$( Get-Location )/bash-aliases-framework/.git/hooks/post-checkout.ps1"
+}
+New-Item -ItemType SymbolicLink -Path "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1" -Target "$(Get-Location)/bash-aliases-framework/.githooks/post-checkout.ps1"
 
-. "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1" "install-script"
+powershell "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1" "install-script"
 
 If (-not (Test-Path "$(Get-Location)/bash-alias-clipboard")) {
     git clone git@github.com:nicolachoquet06250/bash-alias-clipboard.git
 }
 
-Remove-Item -Recurse -Force "$(Get-Location)/bash-alias-clipboard/.git/hooks/post-checkout"
-New-Item -ItemType SymbolicLink -Path "$(Get-Location)/bash-alias-clipboard/.githooks/post-checkout.ps1" -Target "$(Get-Location)/bash-alias-clipboard/.git/hooks/post-checkout.ps1"
-Set-ExecutionPolicy Bypass -command "$(Get-Location)/bash-alias-clipboard/.githooks/post-checkout.ps1"
+If (Test-Path "$(Get-Location)/bash-alias-clipboard/.git/hooks/post-checkout.ps1")
+{
+    Remove-Item -Recurse -Force "$( Get-Location )/bash-alias-clipboard/.git/hooks/post-checkout.ps1"
+}
 
-. "$(Get-Location)/bash-alias-clipboard/.git/hooks/post-checkout" "install-script"
+New-Item -ItemType SymbolicLink -Path "$(Get-Location)/bash-alias-clipboard/.git/hooks/post-checkout.ps1" -Target "$(Get-Location)/bash-alias-clipboard/.githooks/post-checkout.ps1"
+
+powershell "$(Get-Location)/bash-alias-clipboard/.git/hooks/post-checkout" "install-script"
 
 If (-not (Test-Path "$HOME/.bash-aliases-norsys")) {
     $path = ""
     If ($args[1] -eq "install-script") {
         $path = "/bash-aliases-norsys"
     }
-    New-Item -ItemType SymbolicLink -Path "$(Get-Location)$path" -Target "$HOME/.bash-aliases-norsys"
-}
-
-$file_path=".bash-aliases-norsys/.aliases.ps1"
-$source='source "\${HOME}/.bash-aliases-norsys/.aliases.ps1"'
-
-If (-not (Select-String $profile -Pattern $source)) {
-    Set-Content -Path $profile -Value $source
+    New-Item -ItemType SymbolicLink -Path  "$HOME/.bash-aliases-norsys" -Target "$(Get-Location)$path"
 }
